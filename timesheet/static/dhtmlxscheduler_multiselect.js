88008e480ca50658f68c66ce58a162ca25b608c9
Rob McMullen <robm@users.sourceforge.net>
2011-09-08 10:09:59 -0700

    Added dhtmlxScheduler and test html file.  Refs #1

---
+++ timesheet/static/dhtmlxscheduler_multiselect.js
@@ -0,0 +1,45 @@
+/*
+This software is allowed to use under GPL or you need to obtain Commercial or Enterise License
+to use it in not GPL project. Please contact sales@dhtmlx.com for details
+*/
+scheduler.form_blocks.multiselect = {
+    render: function (d) {
+        for (var a = "<div class='dhx_multi_select_" + d.name + "' style='overflow: auto; height: " + d.height + "px; position: relative;' >", b = 0; b < d.options.length; b++) a += "<label><input type='checkbox' value='" + d.options[b].key + "'/>" + d.options[b].label + "</label>", convertStringToBoolean(d.vertical) && (a += "<br/>");
+        a += "</div>";
+        return a
+    },
+    set_value: function (d, a, ev, c) {
+        function h(ev) {
+            for (var c = d.getElementsByTagName("input"), a = 0; a < c.length; a++) c[a].checked = !! ev[c[a].value]
+        }
+        for (var f = d.getElementsByTagName("input"), e = 0; e < f.length; e++) f[e].checked = !1;
+        if (!scheduler._new_event)
+            if (f = [], ev[c.map_to]) {
+                for (var i = ev[c.map_to].split(","), e = 0; e < i.length; e++) f[i[e]] = !0;
+                h(f)
+            }
+            else {
+                // Only call the dhx_crosslink if it's defined, otherwise
+                // assume that the elements have been hardcoded
+                if (c.script_url) {
+                    var g = document.createElement("div");
+                    g.className = "dhx_loading";
+                    g.style.cssText = "position: absolute; top: 40%; left: 40%;";
+                    d.appendChild(g);
+                    dhtmlxAjax.get(c.script_url + "?dhx_crosslink_" + c.map_to + "=" + ev.id + "&uid=" + scheduler.uid(), function (b) {
+                        for (var a = b.doXPath("//data/item"), e = [], f = 0; f < a.length; f++) e[a[f].getAttribute(c.map_to)] = !0;
+                        h(e);
+                        d.removeChild(g)
+                    })
+                }
+                else {
+                    h(f);
+                }
+            }
+    },
+    get_value: function (d) {
+        for (var a = [], b = d.getElementsByTagName("input"), c = 0; c < b.length; c++) b[c].checked && a.push(b[c].value);
+        return a.join(",")
+    },
+    focus: function () {}
+};
